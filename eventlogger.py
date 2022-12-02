e2e1ff49217451581151f9615a3f3a7699deb6da
David Smith <davidmsmith553@gmail.com>
2022-12-02 12:25:33 -0500

    added handler if log folder doesn't exist

--- eventlogger.py
+++ eventlogger.py
@@ -9,6 +9,10 @@ def add_events(event):
 
 def close_log():
     s = datetime.datetime.now().strftime("%Y-%m-%d")
+    isExist = os.path.exists("logs")
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs("logs")
     filename = os.path.join(u.get_local_file_path(),"logs\\weather_alert_log_{}.txt".format(s))
         #"weather_alert_log_{}".format(s),'logs\\')
     with open(filename, 'a+') as file:
