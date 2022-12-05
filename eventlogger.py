3d30a7c7165ab8fa64a860e26780d30b7998747b
David Smith <davidmsmith553@gmail.com>
2022-12-05 13:37:43 -0500

    removed a few comments

--- eventlogger.py
+++ eventlogger.py
@@ -11,10 +11,8 @@ def close_log():
     s = datetime.datetime.now().strftime("%Y-%m-%d")
     isExist = os.path.exists("logs")
     if not isExist:
-        # Create a new directory because it does not exist
         os.makedirs("logs")
     filename = os.path.join(u.get_local_file_path(),"logs\\weather_alert_log_{}.txt".format(s))
-        #"weather_alert_log_{}".format(s),'logs\\')
     with open(filename, 'a+') as file:
             file.write(u.log_events)
     
