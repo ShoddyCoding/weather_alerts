59745a711c587505eb984c8beccd7964ec4f415b
David <David@sunnyburrow.com>
2023-11-01 07:53:07 -0400

    Restructure for freezing events

--- current_weather_capture.py
+++ current_weather_capture.py
@@ -9,16 +9,11 @@ import datetime
 
 def main():
     u.initialize_log()
-    e.add_events("PROGRAM START - Gather Current Weather Data")
     __location__ = u.get_local_file_path()
     config = u.read_json(os.path.join(__location__, 'config.json'))
+    e.add_events("PROGRAM START - Gather Current Weather Data")
     db.setupdbs(config["MYSQL"]["HOSTNAME"],config["MYSQL"]["PORT"]
             ,config["MYSQL"]["WEATHER_USERNAME"],config["MYSQL"]["WEATHER_PASS"])
-    ap = fn.get_air_pollution(config["WEATHER"]["OPENWEATHERAPI"],config["WEATHER"]["LAT"],config["WEATHER"]["LONG"])
-    fn.store_air_pollution(ap)
-    cw = fn.get_current_weather(config["WEATHER"]["OPENWEATHERAPI"],config["WEATHER"]["LAT"],config["WEATHER"]["LONG"])
-    fn.store_current_weather(cw)
-    fn.get_weather_forecast(config["WEATHER"]["OPENWEATHERAPI"],config["WEATHER"]["LAT"],config["WEATHER"]["LONG"])
     d = datetime.datetime.now()
     if d.hour == 15:
         e.add_events("BEGIN - CHECK FOR FREEZING WEATHER EVENTS")
@@ -30,6 +25,11 @@ def main():
                 t.send_message(recepient, freezing_forecasted_condition)
         else:
             e.add_events("INFO: No Freezing Events Found")
+    ap = fn.get_air_pollution(config["WEATHER"]["OPENWEATHERAPI"],config["WEATHER"]["LAT"],config["WEATHER"]["LONG"])
+    fn.store_air_pollution(ap)
+    cw = fn.get_current_weather(config["WEATHER"]["OPENWEATHERAPI"],config["WEATHER"]["LAT"],config["WEATHER"]["LONG"])
+    fn.store_current_weather(cw)
+    fn.get_weather_forecast(config["WEATHER"]["OPENWEATHERAPI"],config["WEATHER"]["LAT"],config["WEATHER"]["LONG"])
     u.remove_old_log_files("logs",30)
     e.close_log()
     db.close_dbs()
